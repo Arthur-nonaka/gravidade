69c4d04a5a8d36c296141d71ed8d55f12ec84c53
Arthur <zenaboa7@gmail.com>
2023-08-16 19:51:31 -0300

    Clica e arrasta

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const container = document.getElementById("container");
+
 var circle;
 var gravity = 0.1;
 var friction = 0.03;
-var ground = 2;
+var ground = 1.7;
 
 function startGame() {
   myGameArea.start();
@@ -12,11 +13,62 @@ function startGame() {
 var myGameArea = {
   canvas: document.createElement("canvas"),
   start: function () {
-    this.canvas.width = 1200;
-    this.canvas.height = 600;
+    const WIDTH = 600,
+      HEIGHT = 600;
+    this.canvas.width = WIDTH;
+    this.canvas.height = HEIGHT;
     this.context = this.canvas.getContext("2d");
     container.appendChild(this.canvas);
     this.interval = setInterval(updateGameArea, 7);
+    var elemLeft = (window.innerWidth - WIDTH) / 2;
+    var elemTop = (window.innerHeight - HEIGHT) / 2;
+    this.ok = false;
+
+    this.canvas.onmousemove = function (event) {
+      var x = event.pageX - elemLeft,
+        y = event.pageY - elemTop;
+
+      if (
+        x <= circle.x + circle.radius &&
+        x >= circle.x - circle.radius &&
+        y <= circle.y + circle.radius &&
+        y >= circle.y - circle.radius
+      ) {
+        document.body.classList.add("cursorAtivado");
+        document.body.classList.remove("cursorDesativado");
+      } else {
+        document.body.classList.add("cursorDesativado");
+        document.body.classList.remove("cursorAtivado");
+      }
+    };
+    this.canvas.onmousedown = function (event) {
+      var x = event.pageX - elemLeft,
+        y = event.pageY - elemTop;
+
+      if (
+        x <= circle.x + circle.radius &&
+        x >= circle.x - circle.radius &&
+        y <= circle.y + circle.radius &&
+        y >= circle.y - circle.radius
+      ) {
+        circle.color = "Yellow";
+        this.ok = true;
+      } else {
+        circle.color = "White";
+      }
+    };
+    this.canvas.addEventListener("mouseup", function (event) {
+      var x = event.pageX - elemLeft,
+        y = event.pageY - elemTop;
+      if (this.ok) {
+        if (x >= circle.x) circle.velocityX += Math.abs(x - circle.x) * 0.1;
+        else if (x < circle.x) circle.velocityX += Math.abs(x - circle.x) * -0.1;
+        if (y >= circle.y) circle.velocityY += Math.abs(y - circle.y) * 0.1;
+        else if (y < circle.y) circle.velocityY += Math.abs(y - circle.y) * -0.1;
+        this.ok = false;
+      }
+      circle.color = "White";
+    });
   },
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -27,13 +79,14 @@ function Circle(radius, x, y) {
   this.radius = radius;
   this.x = x;
   this.y = y;
-  this.velocityY = -4;
-  this.velocityX = 2;
+  this.velocityY = 9.8;
+  this.velocityX = 10;
+  this.color = "White";
   this.update = function () {
     ctx = myGameArea.context;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    ctx.fillStyle = "white";
+    ctx.fillStyle = this.color;
     ctx.fill();
     ctx.stroke();
     if (this.x + this.radius >= myGameArea.canvas.width) {
