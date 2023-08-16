07c9cf860b1509a51a004db1cfa43e8aefda0477
Arthur <zenaboa7@gmail.com>
2023-08-16 11:52:24 -0300

    bordar paredes refinadas

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const container = document.getElementById("container");
 var circle;
 var gravity = 0.1;
 var friction = 0.03;
+var ground = 2;
 
 function startGame() {
   myGameArea.start();
-  circle = new Circle(25, 100, 30);
+  circle = new Circle(25, 200, 10);
 }
 
 var myGameArea = {
   canvas: document.createElement("canvas"),
   start: function () {
-    this.canvas.width = 600;
+    this.canvas.width = 1200;
     this.canvas.height = 600;
     this.context = this.canvas.getContext("2d");
     container.appendChild(this.canvas);
@@ -26,8 +27,8 @@ function Circle(radius, x, y) {
   this.radius = radius;
   this.x = x;
   this.y = y;
-  this.velocityY = 0;
-  this.velocityX = 30;
+  this.velocityY = -4;
+  this.velocityX = 2;
   this.update = function () {
     ctx = myGameArea.context;
     ctx.beginPath();
@@ -35,20 +36,26 @@ function Circle(radius, x, y) {
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.stroke();
-    if(this.x + this.radius >= myGameArea.canvas.width || this.x - this.radius <= 0) {
-        this.velocityX *= -1;
+    if (this.x + this.radius >= myGameArea.canvas.width) {
+      this.velocityX *= -1;
+      this.velocityX /= 2;
+      this.x = myGameArea.canvas.width - this.radius;
+    }
+    if (this.x - this.radius <= 0) {
+      this.velocityX *= -1;
+      this.velocityX /= 2;
+      this.x = 0 + this.radius;
     }
 
     if (this.y + this.radius >= myGameArea.canvas.height) {
-      this.velocityY /= 2;
+      this.velocityY /= ground;
       this.velocityY *= -1;
       this.y = myGameArea.canvas.height - this.radius;
-      if( this.velocityX > -0.1 && this.velocityX < 0.1){
+      if (this.velocityX > -0.1 && this.velocityX < 0.1) {
         this.velocityX = 0;
-      }
-      else if(this.velocityX > 0){
+      } else if (this.velocityX > 0) {
         this.velocityX -= friction;
-      } else if(this.velocityX < 0) {
+      } else if (this.velocityX < 0) {
         this.velocityX += friction;
       }
     } else {
